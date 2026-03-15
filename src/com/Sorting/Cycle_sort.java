9bac54e1014ab3875001588967ccb2f7e81ce47e
harshita porwal <harshitaporwal7@email.com>
2026-03-15 19:34:35 +0530

    sorting

--- src/com/Sorting/Cycle_sort.java
+++ src/com/Sorting/Cycle_sort.java
@@ -1,4 +1,29 @@
 package com.Sorting;
 
+import java.util.Arrays;
+
 public class Cycle_sort {
+    static void main(String[] args) {
+        int[] arr = {7, 4, 5, 1, 3, 2, 6};
+        cycle(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static void cycle(int[] arr) {
+        int i = 0;
+        while(i < arr.length) {
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                swap(arr, i, correct);
+            }else{
+                i++;
+            }
+        }
+    }
 }
--- src/com/Sorting/Insertion_sort.java
+++ src/com/Sorting/Insertion_sort.java
@@ -1,4 +1,29 @@
 package com.Sorting;
 
+import java.util.Arrays;
+
 public class Insertion_sort {
+    static void main(String[] args) {
+        int[] arr = {7, 5 , 9 , 6 , 2};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for(int j = i; j > 0; j--) {
+                if(arr[j] < arr[j-1]) {
+                    swap(arr, j, j-1);
+                }else{
+                    break;
+                }
+            }
+        }
+    }
 }
